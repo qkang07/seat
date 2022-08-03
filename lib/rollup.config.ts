d97d4b971727165f78a2264eb7a7e30a9db2bbe3
qikang <me@qkang.top>
2022-08-03 22:30:44 +0800

    s

--- lib/rollup.config.ts
+++ lib/rollup.config.ts
@@ -1,7 +1,11 @@
 import {defineConfig} from 'rollup'
+import resolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-  input: ['Seat/index.ts','SeatReact/index.tsx'],
+  input: ['Seat/index.ts'],
+  plugins: [
+    resolve()
+  ],
   output: {
     format:'umd',
     dir: 'dist',
